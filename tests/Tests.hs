7084736e77d100abf32900e99e16cce44a148f01
Philipp Chrszon <Philipp.Chszon@mailbox.tu-dresden.de>
2014-06-12 21:28:57 +0200

    Create project

---
+++ tests/Tests.hs
@@ -0,0 +1,9 @@
+-- Tests.hs -- {{
+-- vim: set foldmethod=marker foldmarker={{,}} foldtext=foldtext() :
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+module Main where
+
+main = return ()
+-- }}
