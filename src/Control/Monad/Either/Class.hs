3616cf69a111a6e8e42b1b8a6f8c78d1617207fa
Philipp Chrszon <Philipp.Chszon@mailbox.tu-dresden.de>
2014-07-17 14:24:44 +0200

    Add MonadEither instance for strict Writer monad

--- src/Control/Monad/Either/Class.hs
+++ src/Control/Monad/Either/Class.hs
@@ -12,7 +12,8 @@ import Data.Monoid
 import Control.Monad.Trans
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.State
-import Control.Monad.Trans.Writer
+import Control.Monad.Trans.Writer.Lazy as Lazy
+import Control.Monad.Trans.Writer.Strict as Strict
 
 class (Monad m) => MonadEither e m | m -> e where
     left :: e -> m a
@@ -26,5 +27,9 @@ instance (MonadEither e m) => MonadEither e (ReaderT r m) where
 instance (MonadEither e m) => MonadEither e (StateT s m) where
     left = lift . left
 
-instance (Monoid w, MonadEither e m) => MonadEither e (WriterT w m) where
+instance (Monoid w, MonadEither e m) => MonadEither e (Lazy.WriterT w m) where
     left = lift . left
+
+instance (Monoid w, MonadEither e m) => MonadEither e (Strict.WriterT w m) where
+    left = lift . left
+
