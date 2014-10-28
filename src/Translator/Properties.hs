5217feaa56b30fe169a270438e66d4cfc7dfc708
Philipp Chrszon <Philipp.Chrszon@tu-dresden.de>
2014-10-28 17:41:56 +0100

    Implement translation of properties

--- src/Translator/Properties.hs
+++ src/Translator/Properties.hs
@@ -2,10 +2,24 @@ module Translator.Properties
   ( trnsProperty
   ) where
 
+import Control.Lens
+
+import Symbols
 import Syntax
 
 import Translator.Common
 
 trnsProperty :: Translator LProperty
-trnsProperty = exprs (trnsExpr (const True))
+trnsProperty prop = do
+    root <- view rootFeature
+    if hasSingleConfiguration root
+        then exprs (trnsExpr (const True)) prop
+        else genFilterProperty prop
+
+genFilterProperty :: Translator LProperty
+genFilterProperty (Property ident e l) = do
+    e' <- trnsExpr (const True) e
+    let labelExpr  = LabelExpr initConfLabelIdent noLoc
+        filterExpr = FilterExpr FilterPrint e' (Just labelExpr) noLoc
+    return $ Property ident filterExpr l
 
