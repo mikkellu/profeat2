e84504644c66e8ec6674a5f4170857b35affce97
Philipp Chrszon <Philipp.Chrszon@tu-dresden.de>
2015-01-27 14:15:38 +0100

    Implement pretty printing of results

--- src/Result.hs
+++ src/Result.hs
@@ -1,15 +1,20 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Result
   ( Result(..)
+
   , StateVec
+  , prettyStateVec
 
   , ResultCollection(..)
   , rcStateResults
   , rcFinalResult
   , rcTrace
   , rcLog
+  , prettyResultCollection
 
   , emptyResultCollection
   ) where
@@ -17,15 +22,30 @@ module Result
 import Control.Lens
 
 import Data.Array
-import Data.Sequence ( Seq, empty )
-import Data.Strict.Tuple
+import Data.Foldable                ( toList )
+import Data.Maybe                   ( mapMaybe)
+import Data.Sequence                ( Seq )
+import qualified Data.Sequence as Seq
+import Data.Strict.Tuple            ( (:!:) )
+import qualified Data.Strict.Tuple as ST
 
-import Data.Text ( Text )
+import Data.Text                    ( Text )
+import Data.Text.Lazy               ( fromStrict )
+
+import Text.PrettyPrint.Leijen.Text
+
+import Analysis.VarOrdering
 
 data Result
   = ResultBool !Bool
   | ResultDouble !Double
   deriving (Show)
+
+instance Pretty Result where
+    pretty = \case
+        ResultBool False -> "false"
+        ResultBool True  -> "true"
+        ResultDouble d   -> double d
 
 type StateVec = Array Int Int
 
@@ -38,11 +58,57 @@ data ResultCollection = ResultCollection
 
 emptyResultCollection :: ResultCollection
 emptyResultCollection = ResultCollection
-  { _rcStateResults = empty
+  { _rcStateResults = Seq.empty
   , _rcFinalResult  = ResultBool False
-  , _rcTrace        = empty
-  , _rcLog          = empty
+  , _rcTrace        = Seq.empty
+  , _rcLog          = Seq.empty
   }
+
+prettyResultCollection :: Bool -> VarOrdering -> ResultCollection -> Doc
+prettyResultCollection includeLog vo (ResultCollection srs r tr ls) =
+    "Final result:" <+> pretty r <$>
+    "Results for initial configurations:" <$> prettyStateResults vo srs <$>
+    prettyTrace vo tr <$>
+    if includeLog then prettyLog ls else empty
+
+prettyLog :: Seq Text -> Doc
+prettyLog = vsep . fmap (text . fromStrict) . toList
+
+prettyTrace :: VarOrdering -> Seq StateVec -> Doc
+prettyTrace vo svs
+  | Seq.null svs  = empty
+  | otherwise     = "Counterexample/witness:" <$>
+                    prettyStateVecs vo svs
+
+prettyStateResults :: VarOrdering -> Seq (StateVec :!: Result) -> Doc
+prettyStateResults vo =
+    vsep . fmap (ST.uncurry $ prettyStateResult vo) . toList
+
+prettyStateResult :: VarOrdering -> StateVec -> Result -> Doc
+prettyStateResult vo sv r =
+    parens (prettyStateVec vo sv) <> char '=' <> pretty r
+
+prettyStateVecs :: VarOrdering -> Seq StateVec -> Doc
+prettyStateVecs vo = vsep . fmap (parens . prettyStateVec vo) . toList
+
+prettyStateVec :: VarOrdering -> StateVec -> Doc
+prettyStateVec (VarOrdering vo) =
+    hsep . punctuate comma . mapMaybe (uncurry prettyVal) . zip vo . elems
+
+prettyVal :: (Doc, Range) -> Int -> Maybe Doc
+prettyVal (ident, r) v = case r of
+    RangeFeature
+      | v == 0    -> Nothing
+      | v == 1    -> Just ident
+      | otherwise -> error "Result.prettyVal: illegal value for feature variable"
+    RangeBool
+      | v == 0    -> identDef "false"
+      | v == 1    -> identDef "true"
+      | otherwise -> error "Result.prettyVal: illegal value for boolean variable"
+    Range _ _     -> identDef (int v)
+    RangeInternal -> Nothing
+  where
+    identDef doc = Just $ ident <> char '=' <> doc
 
 makeLenses ''ResultCollection
 
