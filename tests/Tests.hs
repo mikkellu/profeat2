f044b3d0c48bf0483f59e10bba8c229faa60a9f5
Philipp Chrszon <Philipp.Chrszon@tu-dresden.de>
2015-02-04 09:13:44 +0100

    Setup project

---
+++ tests/Tests.hs
@@ -0,0 +1,20 @@
+-- Tests.hs -- {{
+-- vim: set foldmethod=marker foldmarker={{,}} foldtext=foldtext() :
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# LANGUAGE NoMonomorphismRestriction       #-}
+
+module Main
+  ( main
+  ) where
+
+import Test.Tasty
+
+main = defaultMain tests
+-- }}
+-- Tests -- {{
+tests = testGroup "Tests"
+  [
+  ]
+
+-- }}
+
