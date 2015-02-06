9441a70fc0da5503740841e31f25005e5a89c689
Philipp Chrszon <Philipp.Chrszon@tu-dresden.de>
2015-02-06 08:21:50 +0100

    Implement evaluation function

--- src/Data/Bdd.hs
+++ src/Data/Bdd.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase   #-}
+{-# LANGUAGE MultiWayIf   #-}
 {-# LANGUAGE ViewPatterns #-}
 
 -- | Binary Decision Diagrams.
@@ -13,6 +14,7 @@ module Data.Bdd
   , nodeId
   , isTerminal
   , allNodes
+  , eval
   , sat
     -- * Views
   , NodeView(..)
@@ -20,7 +22,7 @@ module Data.Bdd
   ) where
 
 import Data.Monoid
-import Data.Sequence                  ( Seq, (|>) )
+import Data.Sequence                  ( Seq, ViewL(..), (|>), viewl )
 import qualified Data.Sequence as Seq
 import Data.Set                       ( Set )
 import qualified Data.Set as Set
@@ -30,7 +32,7 @@ import Data.Bdd.Internal
 -- | Check if a 'Bdd' node is terminal.
 isTerminal :: Bdd -> Bool
 isTerminal (viewNode -> Terminal _) = True
-isTerminal _                    = False
+isTerminal _                        = False
 
 -- | Get all nodes of a 'Bdd'.
 allNodes :: Bdd -> [Bdd]
@@ -40,19 +42,32 @@ allNodes = Set.elems . go Set.empty where
                         Terminal _     -> ns'
                         Decision _ t e -> go (go ns' t) e
 
+-- | Evaluate the function represented by the given 'Bdd'.
+eval :: Bdd -> Seq Bool -> Bool
+eval = go 0 where
+    go i node val = case viewNode node of
+        Terminal b       -> b
+        Decision var t e ->
+            let (b, vs) = case viewl val of
+                              (v :< vs') -> (v, vs')
+                              EmptyL     -> (False, val)
+            in if | Variable i < var -> go (i + 1) node vs
+                  | b                -> go (i + 1) t vs
+                  | otherwise        -> go (i + 1) e vs
+
 -- | Get all satisfying variable assignments.
 sat :: Int -> Bdd -> Set (Seq Bool)
 sat varCount = go Seq.empty 0 where
-    go val k node = case viewNode node of
+    go val i node = case viewNode node of
         Terminal False -> Set.empty
         Terminal True
-          | k == varCount -> Set.singleton val
+          | i == varCount -> Set.singleton val
           | otherwise     -> step node node
         Decision var t e
-          | Variable k < var -> step node node
+          | Variable i < var -> step node node
           | otherwise        -> step t e
       where
-        step t e = go (val |> True)  (k + 1) t <> go (val |> False) (k + 1) e
+        step t e = go (val |> True) (i + 1) t <> go (val |> False) (i + 1) e
 
 
 -- | A 'Bdd' node is either terminal (labeled with 0 or 1) or it is
