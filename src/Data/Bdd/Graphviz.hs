3fabc24a7c9714da78de712f24dfb622535eaa51
Philipp Chrszon <Philipp.Chrszon@tu-dresden.de>
2015-02-10 08:25:12 +0100

    Implement dot export with variable names

--- src/Data/Bdd/Graphviz.hs
+++ src/Data/Bdd/Graphviz.hs
@@ -3,10 +3,14 @@
 -- | Functions for rendering 'Bdd's and 'Sobdd's to Graphviz dot format.
 module Data.Bdd.Graphviz
   ( renderSobdd
+  , renderSobddWithNames
   , renderBdd
+  , renderBddWithNames
   ) where
 
-import Data.Text.Lazy ( Text )
+import Data.Map                     ( Map )
+import qualified Data.Map as Map
+import Data.Text.Lazy               ( Text )
 
 import Text.PrettyPrint.Leijen.Text
 
@@ -15,26 +19,41 @@ import Data.Bdd.Internal
 
 -- | Render all nodes of an 'Sobdd'.
 renderSobdd :: Text -> Sobdd -> Doc
-renderSobdd name (Sobdd ns) = bdd name ns
+renderSobdd = renderSobddWithNames (Map.empty :: Map Variable ())
+
+-- | Render all nodes of an 'Sobdd' and label them with their variable
+-- name. If a 'Variable' is not contained in the given 'Map', its index is
+-- used to label the corresponding nodes.
+renderSobddWithNames :: Pretty a => Map Variable a -> Text -> Sobdd -> Doc
+renderSobddWithNames names name (Sobdd ns) = bdd names name ns
 
 -- | Render all nodes of a 'Bdd'.
 renderBdd :: Text -> Bdd -> Doc
-renderBdd name n = bdd name (allNodes n)
+renderBdd = renderBddWithNames (Map.empty :: Map Variable ())
 
-bdd :: Text -> [Bdd] -> Doc
-bdd name ns = "digraph" <+> text name <+> lbrace <$>
-    indent 4 (nodes ns) <$> rbrace
+-- | Render all nodes of a 'Bdd' and label them with their variable
+-- name. If a 'Variable' is not contained in the given 'Map', its index is
+-- used to label the corresponding nodes.
+renderBddWithNames :: Pretty a => Map Variable a -> Text -> Bdd -> Doc
+renderBddWithNames names name n = bdd names name (allNodes n)
 
-nodes :: [Bdd] -> Doc
-nodes = vsep . map node
+bdd :: Pretty a => Map Variable a -> Text -> [Bdd] -> Doc
+bdd names name ns = "digraph" <+> text name <+> lbrace <$>
+    indent 4 (nodes names ns) <$> rbrace
 
-node :: Bdd -> Doc
-node n = case viewNode n of
-    Terminal _   -> terminal nid
-    Decision (Variable v) t e ->
-        nid <+> brackets ("label=" <> dquotes (int v)) <$>
-        nid <+> edge <+> int (nodeId t) <> semi <$>
-        nid <+> edge <+> int (nodeId e) <+> brackets "style=dashed" <> semi
+nodes :: Pretty a => Map Variable a -> [Bdd] -> Doc
+nodes names = vsep . map (node names)
+
+node :: Pretty a => Map Variable a -> Bdd -> Doc
+node names n = case viewNode n of
+    Terminal _       -> terminal nid
+    Decision var t e ->
+        let label = case Map.lookup var names of
+                        Just x  -> pretty x
+                        Nothing -> int (getVariable var)
+        in nid <+> brackets ("label=" <> dquotes label) <$>
+           nid <+> edge <+> int (nodeId t) <> semi <$>
+           nid <+> edge <+> int (nodeId e) <+> brackets "style=dashed" <> semi
   where
     nid = int (nodeId n)
 
